3d85bb02aa80d63d9a75ac5d8c6c25f88b447a19
malaprince <malaminebah91@gmail.com>
2020-07-12 16:45:59 +0200

    nav bar mais a voir apres debugg

---
+++ script/popup.js
@@ -0,0 +1,6 @@
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+})
