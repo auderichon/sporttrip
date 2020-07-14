cede2050a9505ff88b1242ebfceee0a8d5bc0656
Aude Richon <auderichon@gmail.com>
2020-07-14 19:26:44 +0200

    css changes on home and forms

--- bin/Activities.seeds.js
+++ bin/Activities.seeds.js
@@ -3,57 +3,96 @@
 // const ActivityModel = require("./../models/Activities");
 
 const ActivitiesList = [
-   {
-      activityName: "Paris - Lyon by bike",
-      date: "2020-01-01",
-      location: {
-         city: "Paris",
-         country: "France",
-      },
-      sport: "Cycling", //cycling
-      requiredLevel: "experienced",
-      duration: "3 days",
-      maxNbParticipants: 4,
-      creator: "Aude", //Aude
-      participants: [
-         { participantID: "Mala" },
-         { participantID: "Audrey" },
-      ],
-      description: "A very good trip !",
-   },
-   {
-      activityName: "Climbing over Interlaken",
-      date: "2020-01-30",
-      location: {
-         city: "Interlaken",
-         country: "Switzerland",
-      },
-      sport: "Climbing", //climbing
-      requiredLevel: "all",
-      duration: "1/2 day",
-      maxNbParticipants: 4,
-      creator: "Mala", //Mala
-      participants: [
-         { participantID: "Aude" },
-         { participantID: "Audrey" },
-      ],
-      description: "Perfect trip",
-   },
-   {
-      activityName: "Sailing in Corsica",
-      date: "2020-07-23",
-      location: {
-         city: "Bastia",
-         country: "France",
-      },
-      sport: "Sailing", //sailing
-      requiredLevel: "intermediate",
-      duration: "full day",
-      maxNbParticipants: 6,
-      creator: "Audrey", //Audrey
-      participants: [{ participantID: "Mala" }, { participantID: "Aude" }],
-      description: "Cool spot !!!",
-   },
+  {
+    activityName: "Paris - Lyon by bike",
+    date: "2020-01-01",
+    location: {
+      city: "Paris",
+      country: "France",
+    },
+    sport: "Cycling", //cycling
+    requiredLevel: "experienced",
+    duration: "3 days",
+    maxNbParticipants: 4,
+    creator: "Aude", //Aude
+    participants: [{ participantID: "Mala" }, { participantID: "Audrey" }],
+    description: "A very good trip !",
+  },
+  {
+    activityName: "Climbing over Interlaken",
+    date: "2020-01-30",
+    location: {
+      city: "Interlaken",
+      country: "Switzerland",
+    },
+    sport: "Climbing", //climbing
+    requiredLevel: "all",
+    duration: "1/2 day",
+    maxNbParticipants: 4,
+    creator: "Mala", //Mala
+    participants: [{ participantID: "Aude" }, { participantID: "Audrey" }],
+    description: "Perfect trip",
+  },
+  {
+    activityName: "Sailing in Corsica",
+    date: "2020-07-23",
+    location: {
+      city: "Bastia",
+      country: "France",
+    },
+    sport: "Sailing", //sailing
+    requiredLevel: "intermediate",
+    duration: "full day",
+    maxNbParticipants: 6,
+    creator: "Audrey", //Audrey
+    participants: [{ participantID: "Mala" }, { participantID: "Aude" }],
+    description: "Cool spot !!!",
+  },
+  {
+    activityName: "Paris - Lyon by bike",
+    date: "2020-01-01",
+    location: {
+      city: "Paris",
+      country: "France",
+    },
+    sport: "Cycling", //cycling
+    requiredLevel: "experienced",
+    duration: "3 days",
+    maxNbParticipants: 4,
+    creator: "Aude", //Aude
+    participants: [{ participantID: "Mala" }, { participantID: "Audrey" }],
+    description: "A very good trip !",
+  },
+  {
+    activityName: "Climbing over Interlaken",
+    date: "2020-01-30",
+    location: {
+      city: "Interlaken",
+      country: "Switzerland",
+    },
+    sport: "Climbing", //climbing
+    requiredLevel: "all",
+    duration: "1/2 day",
+    maxNbParticipants: 4,
+    creator: "Mala", //Mala
+    participants: [{ participantID: "Aude" }, { participantID: "Audrey" }],
+    description: "Perfect trip",
+  },
+  {
+    activityName: "Sailing in Corsica",
+    date: "2020-07-23",
+    location: {
+      city: "Bastia",
+      country: "France",
+    },
+    sport: "Sailing", //sailing
+    requiredLevel: "intermediate",
+    duration: "full day",
+    maxNbParticipants: 6,
+    creator: "Audrey", //Audrey
+    participants: [{ participantID: "Mala" }, { participantID: "Aude" }],
+    description: "Cool spot !!!",
+  },
 ];
 
 module.exports = ActivitiesList;
