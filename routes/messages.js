367180e40a83384fb7f1a741619ec8cbf3c741d9
malaprince <malaminebah91@gmail.com>
2020-07-15 20:20:25 +0200

    mgs update

--- routes/messages.js
+++ routes/messages.js
@@ -5,55 +5,96 @@ const sportModel = require("../models/Sports");
 const userModel = require("../models/Users");
 const conversationModel = require("../models/Conversation");
 
-router.get("/",(req,res,next) =>{
-    console.log( ">>>>>>>>>" ,req.session.currentUser)
-    conversationModel 
-    .find({users: req.session.currentUser._id})
-    .then((conversations)=>{
-        
-        res.render("message/message-profile", {title:"New message", conversations});
-    })
-    .catch(next)
+router.get("/", (req, res, next) => {
+    console.log("++++++++++++++", req.session.currentUser._id)
+    const user = req.session.currentUser._id;
+    Promise.all([
+        conversationModel
+    
+            .find({ users: { $elemMatch: { user } } })
+            .populate("users"),
+        userModel.find()
+
+    ])
+        .then((conversations) => {
+            console.log("======", conversations);
+
+
+            res.render("message/message-profile", { title: "Messages", conversations });
+        })
+        .catch(next)
 });
-router.get("/:id",(req,res,next) =>{
-    userModel.findById(req.params.id).then(userDocument => {
-        res.render("message/message-profile", {recipient: userDocument})
-    }).catch(next)
+// router.get("/:id",(req,res,next) =>{
+//     userModel.findById(req.params.id).then(userDocument => {
+//         res.render("message/message-profile", {recipient: userDocument})
+//     }).catch(next)
 
-})
+// })
 
 
-router.get("/:id", (req,res,next) =>{
+// router.get("/:id", (req,res,next) =>{
+//     conversationModel
+//     .find(req.session.currentUser._id, userDocument)
+//     .then((dbres) =>{
+//         if(!dbres){
+
+//         }
+//     })
+
+// })
+router.post("/:user1id/:user2id", (req, res, next) => {
+
+    const users = [req.params.user1id, req.params.user2id];
     conversationModel
-    .find(req.session.currentUser._id, userDocument)
-    .then((dbres) =>{})
-    
-})
-router.post("/:id",(req,res, next) =>{
-    const {users,message} = req.body;
+        .findOne({ $or: [{ users: [req.params.user1id, req.params.user2id] }, { users: [req.params.user2id, req.params.user1id] }] })
+        .then((dbres) => {
+            console.log("=========", dbres);
+            if (dbres === null) {
+                conversationModel.create({
+                    users,
+                    messages: [{
+                        user: req.params.user2id,
+                        content: req.body.content,
+                        date: Date.now()
 
-//regarder si une conversation existe déjà entre ces deux users, du coup update l'array de messages, sinon creer une conversation
+                    }]
+                }).then(req.flash("success", "Conversation successfully created"),
+                    res.redirect("/"))
+                    .catch(next)
+            }
+            else {
 
-    conversationModel
-    .create({
-        users:req.session.currentUser._id,
-        message,
+                conversationModel.findByIdAndUpdate(dbres._id, {
+                    $push: {
+                        messages: {
+                            user: req.params.user2id,
+                            content: req.body.content,
+                            date: Date.now()
 
-    })
-    .then(() => {
-        req.flash("success", "message successfully created");
-        res.redirect("/");
-      })
-      .catch(next);
-  });
+                        }
+                    }
 
-    
+                })
+                    .then(req.flash("success", "Message successfully added"),
+                        res.redirect("/"))
+                    .catch(next)
+            }
+
+        })
+
+
+
+
+        .catch(next)
+});
+
+
 
 
 router.get("/delete/:id", (req, res, next) => {
-    conversationModel 
-      .findByIdAndRemove(req.params.id)
-      .then(() => res.redirect("/")) //(res.redirect mymessage)
-      .catch(next);
-  });
+    conversationModel
+        .findByIdAndRemove(req.params.id)
+        .then(() => res.redirect("/")) //(res.redirect mymessage)
+        .catch(next);
+});
 module.exports = router;
