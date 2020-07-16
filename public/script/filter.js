4f699f1e12e7258b98c34298559eebb4120898eb
Aude Richon <auderichon@gmail.com>
2020-07-16 23:25:01 +0200

    ee

--- public/script/filter.js
+++ public/script/filter.js
@@ -37,24 +37,18 @@ form.onchange = function (event) {
 
       response.data.forEach((res) => {
         activityList.innerHTML += `<div class="each-activity">
-          <a href="/activity/${res._id}"><img src="${
-          res.sport.picture
-        }" alt=""></a>
+          <a href="/activity/${res._id}"><img src="${res.sport.picture}" alt=""></a>
           <h4>${res.activityName}</h4>
           <a href="/user/profile/${res.creator._id}">
               <div class="activity-creator">
                   <p>with ${res.creator.firstName}</p>
-                  <img src="${res.creator.picture}" alt="${
-          res.creator.firstName
-        } pic">
+                  <img src="${res.creator.picture}" alt="${res.creator.firstName} pic">
               </div>
           </a>
           <p>Level: ${res.requiredLevel}</p>
           <div class="time">
               <p><i class="fas fa-stopwatch"></i>${res.duration}</p>
-              <p><i class="fas fa-calendar-day"></i>${res.date.toLocaleDateString(
-                "en-GB"
-              )}</p>
+              <p><i class="fas fa-calendar-day"></i>${res.date}</p>
           </div>
           <div class="activity-button"
               <a href="/activity/ ${res._id} ">Check it out!</a>
