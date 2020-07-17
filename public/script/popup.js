d442910a643fdef5abbc6780537e8fef022a5a4c
malaprince <malaminebah91@gmail.com>
2020-07-17 09:51:30 +0200

    no socket

--- public/script/popup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("button").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "flex";
-})
-document.querySelector(".close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
-})
